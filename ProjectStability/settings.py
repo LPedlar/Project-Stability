3e6c1918647a2c79b8d4ca9f5affd3f6a0a5b626
Aidan <aidschmidgall@gmail.com>
2023-04-19 15:01:22 -0500

    stuff

--- ProjectStability/settings.py
+++ ProjectStability/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
-
+# get to work
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.employer',
+    
 ]
 
 MIDDLEWARE = [
